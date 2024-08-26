b1d0059ee4e44355785c0f45a476a769855efe1f
MariamwAmer <mariamwamer9@gmail.com>
2024-08-26 15:11:52 +0300

    Password

--- Signin.kt
+++ Signin.kt
@@ -1,2 +1,26 @@
-class Signin {
+@androidx.compose.runtime.Composable
+
+fun password() {
+    OutlinedTextField(
+        value = password,
+        onValueChange = { newText -> password = newText },
+        label = { Text(text = stringResource(id = R.string.password) ,
+            Modifier.alpha(0.6f),
+            color = colorResource(id =  R.color.Gray),
+            textAlign = TextAlign.Start)},
+        modifier = Modifier
+            .size(350.dp, 100.dp)
+            .padding(top = 30.dp, start = 30.dp),
+        visualTransformation = PasswordVisualTransformation(),
+        trailingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_disabled_visible_24),
+                contentDescription = "Password isn't visible",
+            )
+        }
+
+    )
+
 }
+
+
