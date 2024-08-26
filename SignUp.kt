15ba9440fc96c92aa4a5590b4fcfdfbc0cf31790
mohaned-emad <mohanedemad11@gmail.com>
2024-08-26 15:32:20 +0300

    added password field

--- SignUp.kt
+++ SignUp.kt
@@ -1,7 +1,15 @@
 class SignUp {
     OutlinedTextField(
-        modifier = modifier.paddin(all = 8.dp)
-    ){
-
-    }
+        value = password,
+        onValueChange = { newText -> password = newText },
+        label = {
+            Text(text = stringResource(id = R.string.password),
+            Modifier.alpha(0.6f),
+            textAlign = TextAlign.Start)
+                },
+        modifier = Modifier
+            .size(350.dp, 100.dp)
+            .padding(top = 30.dp, start = 30.dp),
+    visualTransformation = PasswordVisualTransformation(),
+    )
 }
